eadc7aabf309f4a8ccf827c11de2f7b753f79b24
吉田大輝 <bob181219@yahoo.co.jp>
2018-07-18 18:47:51 +0900

    Describe setup.pl

--- setup.pl
+++ setup.pl
@@ -1,27 +1,90 @@
 use strict;
 use warnings;
+use File::Copy;
 
-sub error {
-	my ($message) = @_;
-	die "Error: " . $message . "\n";
+sub menu {
+	my (@selects) = @_;
+
+	for(my $i = 1; $i <= 55; ++$i) {
+		print "*";
+	}
+	print "\n";
+
+	my $n = 1;
+	foreach (@selects) {
+		print "$n. $_" . "\n";
+		++$n;
+	}
+
+	for(my $i = 1; $i <= 55; ++$i) {
+		print "*";
+	}
+	print "\n";
+
+	while(1) {
+		print ">";
+		my $n = <STDIN>;
+		chomp $n;
+		$n += 0;
+
+		if($n > 0 and $n <= @selects) {
+			return $n;
+		}
+	}
 }
 
-print "Paths:\n";
-my @paths = <STDIN>;
-unless(@paths == 0) {
-	open(PATHS_FILE, ">", "./data/PATH") or &error("cannot open path file");
+sub failed {
+	my ($message) = @_;
+	die "Error: $message\n";
+}
+
+sub setup_new {
+	print "Paths:" . "\n";
+	my @paths = <STDIN>;
+
+	print "Executable Extensions:" . "\n";
+	my @executable_extensions = <STDIN>;
+
+	open(PATHS_FILE, "> data/PATH") or &failed("failed open path file");
 	foreach (@paths) {
 		print PATHS_FILE $_;
 	}
-}
 
-print "\n";
-
-print "Executable Extensions:\n";
-my @executable_extensions = <STDIN>;
-unless(@executable_extensions == 0) {
-	open(EXECUTABLE_EXTENSIONS_FILE, ">", "./data/EXECUTABLE_EXTENSIONS") or &error("cannot open executable-extensions file");
+	open(EXECUTABLE_EXTENSIONS_FILE, "> data/EXECUTABLE_EXTENSIONS") or &failed("failed open executable-extensions file");
 	foreach (@executable_extensions) {
 		print EXECUTABLE_EXTENSIONS_FILE $_;
 	}
+}
+
+sub setup_old {
+	print "Other HyperCommandLineShell's version:" . "\n";
+	my $selects = &menu(("1.0.0 - 2.1.0"), ("2.2.0 - 2.3.0"));
+
+	print "Other HyperCommandLineShell's directory: ";
+	my $src_dir = <STDIN>;
+	chomp $src_dir;
+
+	if($selects == 1) {
+		copy("$src_dir/data/PATH", "./data/PATH") or &failed("failed copy path file");
+
+		print "Executable Extensions:" . "\n";
+		my @executable_extensions = <STDIN>;
+
+		open(EXECUTABLE_EXTENSIONS_FILE, "> data/EXECUTABLE_EXTENSIONS") or &failed("failed open executable-extensions file");
+		foreach (@executable_extensions) {
+			print EXECUTABLE_EXTENSIONS_FILE $_;
+		}
+	} else {
+		copy("$src_dir/data/PATH", "./data/PATH") or &failed("failed copy path file");
+		copy("$src_dir/data/EXECUTABLE_EXTENSIONS", "./data/EXECUTABLE_EXTENSIONS") or &failed("failed copy executable-extensions file");
+	}
+}
+
+print "HyperCommandLineShell Setup:" . "\n";
+my $select = &menu(("Setup with new settings", "Setup with other HyperCommandLineShell's settings"));
+
+if($select == 1) {
+	&setup_new;
+} elsif($select == 2) {
+	&setup_old;
 }
